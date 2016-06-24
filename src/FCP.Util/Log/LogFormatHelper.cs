34fcc3c6ca63b7116dea2512572f384281832859
万里涛 <wanlitao_3055601@outlook.com>
2016-06-24 16:31:45 +0800

    remove unuse code

--- src/FCP.Util/Log/LogFormatHelper.cs
+++ src/FCP.Util/Log/LogFormatHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace FCP.Util
 {
@@ -8,8 +7,6 @@ namespace FCP.Util
     /// </summary>
     public static class LogFormatHelper
     {
-        private static string LogPrefix = "Log";
-
         /// <summary>
         /// 格式化异常信息
         /// </summary>
@@ -22,29 +19,6 @@ namespace FCP.Util
                 return string.Format("{0}{1}{2}{3}", ex.Message, Environment.NewLine, ex.StackTrace, Environment.NewLine);
             }
             return string.Empty;
-        }
-
-        /// <summary>
-        /// 文本文件日志记录信息函数
-        /// </summary>
-        /// <param name="message">具体的信息</param>
-        public static void WriteTxtLog(string message)
-        {
-            try
-            {
-                lock (LogPrefix)
-                {
-                    if (!Directory.Exists(System.AppDomain.CurrentDomain.BaseDirectory + "\\LOG\\"))
-                        Directory.CreateDirectory(System.AppDomain.CurrentDomain.BaseDirectory + "\\LOG\\");
-                    using (StreamWriter sw = File.AppendText(System.AppDomain.CurrentDomain.BaseDirectory + "\\LOG\\" + LogPrefix + DateTime.Now.ToString("yyyyMMdd") + ".log"))
-                    {
-                        sw.WriteLine(message);
-                    }
-                }
-            }
-            catch (Exception)
-            {
-            }
         }
     }
 }
