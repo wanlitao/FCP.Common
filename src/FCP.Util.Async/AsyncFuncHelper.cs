18ad613a7e80f3fe5755077c99d3b29a34ad44a0
万里涛 <wanlitao_3055601@outlook.com>
2016-07-25 19:31:17 +0800

    add FCP Async Util library

---
+++ src/FCP.Util.Async/AsyncFuncHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FCP.Util.Async
+{
+    public static class AsyncFuncHelper
+    {
+        private static readonly TaskFactory s_myTaskFactory = new TaskFactory(CancellationToken.None,
+            TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return s_myTaskFactory.StartNew(func)
+                .Unwrap().GetAwaiter().GetResult();
+        }
+
+        public static void RunSync(Func<Task> func)
+        {
+            s_myTaskFactory.StartNew(func)
+              .Unwrap().GetAwaiter().GetResult();
+        }
+    }
+}
