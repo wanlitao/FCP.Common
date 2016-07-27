1deef15e1e3a85270d9e974e49ad17ad92ecdf1e
万里涛 <wanlitao_3055601@outlook.com>
2016-07-27 15:55:05 +0800

    add dictionary extensions

---
+++ src/FCP.Util/Extension/DictionaryExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace FCP.Util
+{
+    /// <summary>
+    /// 字典 扩展方法
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// 批量移除指定键的元素
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        public static IDictionary<TKey, TValue> ChainRemove<TKey, TValue>(this IDictionary<TKey, TValue> dict, params TKey[] keys)
+        {
+            if (dict != null)
+            {
+                if (keys.isNotEmpty())
+                {
+                    foreach(var key in keys)
+                    {
+                        dict.Remove(key);
+                    }
+                }
+            }
+
+            return dict;
+        }
+    }
+}
