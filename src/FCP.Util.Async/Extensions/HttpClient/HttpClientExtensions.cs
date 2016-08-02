61b4e919fa58cc5dc0d210fb526c1de89d307388
万里涛 <wanlitao_3055601@outlook.com>
2016-08-02 20:18:54 +0800

    add HttpClient extension methods

---
+++ src/FCP.Util.Async/Extensions/HttpClient/HttpClientExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FCP.Util.Async
+{
+    public static class HttpClientExtensions
+    {
+        #region Post Extensions
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient client, string requestUri, T value)
+        {
+            return client.PostAsJsonAsync(requestUri, value, null);
+        }
+
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient client, string requestUri, T value, Encoding encoding)
+        {
+            return client.PostAsJsonAsync(requestUri, value, encoding, CancellationToken.None);
+        }
+
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient client, string requestUri, T value,
+            Encoding encoding, CancellationToken cancellationToken)
+        {
+            var content = new JsonStringContent<T>(value, encoding);
+
+            return client.PostAsync(requestUri, content, cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient client, Uri requestUri, T value)
+        {
+            return client.PostAsJsonAsync(requestUri, value, null);
+        }
+
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient client, Uri requestUri, T value, Encoding encoding)
+        {
+            return client.PostAsJsonAsync(requestUri, value, encoding, CancellationToken.None);
+        }
+
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient client, Uri requestUri, T value,
+            Encoding encoding, CancellationToken cancellationToken)
+        {
+            var content = new JsonStringContent<T>(value, encoding);
+
+            return client.PostAsync(requestUri, content, cancellationToken);
+        }
+        #endregion
+    }
+}
---
+++ src/FCP.Util.Async/Extensions/HttpClient/JsonStringContent.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace FCP.Util.Async
+{
+    public class JsonStringContent : StringContent
+    {
+        private const string defaultMediaType = "application/json";
+
+        internal JsonStringContent(string content)
+            : this(content, null)
+        { }
+
+        internal JsonStringContent(string content, Encoding encoding)
+            : base(content, encoding, defaultMediaType)            
+        { }
+    }
+
+    public class JsonStringContent<T> : JsonStringContent
+    {
+        internal JsonStringContent(T value)
+            : this(value, null)
+        { }
+
+        internal JsonStringContent(T value, Encoding encoding)
+            : base(GetSerializeJson(value), encoding)
+        { }
+
+        protected static string GetSerializeJson(object value)
+        {
+            if (value == null)
+                return string.Empty;
+
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
