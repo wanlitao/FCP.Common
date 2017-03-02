b0d29f58b03effd8e38f6ed663f9fb00daf110e2
万里涛 <wanlitao_3055601@outlook.com>
2017-03-02 15:30:04 +0800

    add newtonsoft.json raw string converter

---
+++ src/FCP.Util/Serializer/JsonNet/RawStringJsonConverter.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace FCP.Util
+{
+    public class RawStringJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(string).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            return reader.Value;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            writer.WriteRawValue(value.ToString());
+        }
+    }
+}
